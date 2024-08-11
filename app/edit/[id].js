c429a39ca92e535117ecbc3ea4938f9c74a9edb9
Heyson <heysonbr@outlook.es>
2024-08-11 16:46:37 +0200

    Actualizar estilos de la barra de búsqueda en la pantalla principal

--- app/edit/[id].js
+++ app/edit/[id].js
@@ -62,41 +62,43 @@ export default function Edit() {
           headerLeft: () => null,
         }}
       />
-      <Text className="font-bold text-xl pb-2">Editar libro</Text>
+      <Text className="font-bold text-2xl pb-2 text-white">Editar libro</Text>
 
-      <Text>Titulo</Text>
+      <Text className="text-white text-xl font-semibold">Titulo</Text>
       <TextInput
-        className="border border-gray-400 rounded-lg p-2 w-80 mb-2"
+        className="border border-gray-400 bg-white rounded-lg p-2 w-80 mb-2"
         value={bookInfo.titulo}
         onChangeText={(value) => handleChange("titulo", value)}
         placeholder="Introduce el título"
       />
-      <Text>Páginas</Text>
+      <Text className="text-white text-xl font-semibold">Páginas</Text>
       <TextInput
-        className="border border-gray-400 rounded-lg p-2 w-80 mb-2"
+        className="border border-gray-400  bg-white rounded-lg p-2 w-80 mb-2"
         value={(bookInfo.paginas ?? "").toString()}
         onChangeText={(value) => handleChange("paginas", value)}
         placeholder="Introduce el número de páginas"
         keyboardType="numeric"
       />
-      <Text>ISBN</Text>
+      <Text className="text-white text-xl font-semibold">ISBN</Text>
       <TextInput
-        className="border border-gray-400 rounded-lg p-2 w-80 mb-2"
+        className="border border-gray-400  bg-white rounded-lg p-2 w-80 mb-2"
         value={(bookInfo.ISBN ?? "").toString()}
         onChangeText={(value) => handleChange("ISBN", value)}
         placeholder="Introduce el ISBN"
         keyboardType="numeric"
       />
-      <Text>Géneros</Text>
+      <Text className="text-white text-xl font-semibold">Géneros</Text>
       <TextInput
-        className="border border-gray-400 rounded-lg p-2 w-80 mb-2"
+        className="border border-gray-400  bg-white rounded-lg p-2 w-80 mb-2"
         value={bookInfo.genero}
         onChangeText={(value) => handleChange("genero", value)}
         placeholder="Introduce el géneros separados por comas ,"
       />
-      <Text>Sinopsis: {(bookInfo.sinopsis ?? "").length} letras </Text>
+      <Text className="text-white text-xl font-semibold">
+        Sinopsis: {(bookInfo.sinopsis ?? "").length} letras{" "}
+      </Text>
       <TextInput
-        className="border border-gray-400 rounded-lg p-2 w-80 mb-2"
+        className="border border-gray-400  bg-white rounded-lg p-2 w-80 mb-2"
         value={bookInfo.sinopsis}
         onChangeText={(value) => handleChange("sinopsis", value)}
         placeholder="Introduce la sinopsis"
