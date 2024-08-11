d370417064732a740a3e4f580d65bec91dacc374
Heyson <heysonbr@outlook.es>
2024-08-11 19:26:59 +0200

    Actualizar estilos de la barra de búsqueda en la pantalla principal

--- app/about.js
+++ app/about.js
@@ -5,16 +5,16 @@ import { Stack } from "expo-router";
 
 export default function About() {
   return (
-    <ScrollView className="pt-4 bg-white">
+    <ScrollView className="pt-4 px-8 bg-blue-950">
       <Stack.Screen
         options={{
           headerTitle: "Sobre nosotros",
           headerLeft: () => null,
         }}
       />
-      <Link asChild href="/"></Link>
-      <Text className="text-black font-bold mb-8 text-2xl ">About</Text>
-      <Text className="text-black text-black/90 mb-4">
+
+      <Text className="text-white font-bold mb-8 text-2xl ">About</Text>
+      <Text className="text-white  mb-4">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
         Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies
         sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius
@@ -26,43 +26,7 @@ export default function About() {
         Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit
         sodales. Vestibulum ante ipsum primis in faucibus orci
       </Text>
-      <Text className="text-black text-black/90 mb-4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
-        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies
-        sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius
-        a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy
-        molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
-        Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium
-        a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra
-        tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.
-        Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit
-        sodales. Vestibulum ante ipsum primis in faucibus orci
-      </Text>
-      <Text className="text-black text-black/90 mb-4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
-        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies
-        sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius
-        a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy
-        molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
-        Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium
-        a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra
-        tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.
-        Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit
-        sodales. Vestibulum ante ipsum primis in faucibus orci
-      </Text>
-      <Text className="text-black text-black/90 mb-4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
-        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies
-        sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius
-        a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy
-        molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
-        Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium
-        a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra
-        tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.
-        Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit
-        sodales. Vestibulum ante ipsum primis in faucibus orci
-      </Text>
-      <Text className="text-black text-black/90 mb-4">
+      <Text className="text-white text-white/90 mb-4">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
         Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies
         sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius
